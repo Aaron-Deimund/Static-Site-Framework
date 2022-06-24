1a29ddc09826f2d50d106b3cac64bb04cf10f9a4
Aaron Deimund <acdeimund@gmail.com>
2022-06-24 14:51:38 -0500

    Setting things up to use javascript modules.

---
+++ js/data-access/DataRepo.js
@@ -0,0 +1,10 @@
+export default class DataRepo{
+    constructor(message){
+        this.message = message;
+    }
+
+    alert(){
+        return this.message;
+    }
+    
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import DataRepo from "./data-access/DataRepo.js";
+
+const repo = new DataRepo("Data!!!");
+console.log(repo.alert());
